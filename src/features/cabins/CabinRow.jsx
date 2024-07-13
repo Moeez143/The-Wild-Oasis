17fe11d7c4f59da218e4e65815764316762df8f3
Moeez Nadeem <moeeznadeem143@gmail.com>
2024-07-13 11:55:41 +0500

    cabin data fetching

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {formatCurrency} from '../../utils/helpers';
 
 const TableRow = styled.div`
   display: grid;
@@ -18,7 +19,7 @@ const Img = styled.img`
   aspect-ratio: 3 / 2;
   object-fit: cover;
   object-position: center;
-  transform: scale(1.5) translateX(-7px);
+  transform: scale(1.5) translateX(-3px);
 `;
 
 const Cabin = styled.div`
@@ -38,3 +39,20 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+function CabinRow({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button>Delete</button>
+    </TableRow>
+  );
+}
+
+export default CabinRow;
