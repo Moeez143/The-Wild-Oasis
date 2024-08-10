dd07000fb41e1f074299cd3a874e4378df74f5db
Moeez Nadeem <moeeznadeem143@gmail.com>
2024-08-10 23:22:33 +0500

    API side Sorting

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -5,20 +5,25 @@ import { useSearchParams } from "react-router-dom";
 export function useBookings() {
   const [searchParams] = useSearchParams();
 
-  //fillter here
+  //Fillter Here
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
 
+  //Sorting Here
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isPending: isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
