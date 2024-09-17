48774cd0b24c88a09362dd44735468584e801e2a
Moeez Nadeem <moeeznadeem143@gmail.com>
2024-09-17 21:14:41 +0500

    Pie Chart

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -92,7 +92,10 @@ function SalesChart({ bookings, numDays }) {
 
   return (
     <StyledSalesChart>
-      <Heading as="h2">Sale</Heading>
+      <Heading as="h2">
+        Sales from {format(allDate.at(0), "MMM dd yyyy")} &mdash;{" "}
+        {format(allDate.at(-1), "MMM dd yyyy")}
+      </Heading>
 
       <ResponsiveContainer width="100%" height={300}>
         <AreaChart data={data}>
