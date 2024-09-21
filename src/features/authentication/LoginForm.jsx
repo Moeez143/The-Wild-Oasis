a58e78a7d6af09b825f648b62ffbcdaf1d2582e1
Moeez Nadeem <moeeznadeem143@gmail.com>
2024-09-21 15:35:48 +0500

    final touches

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("tamic99263@barakal.com");
-  const [password, setPassword] = useState("test@test.com");
+  const [email, setEmail] = useState("test@test.com");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
